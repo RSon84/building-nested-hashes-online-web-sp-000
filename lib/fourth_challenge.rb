4a4a992025b52a8d4dbaf557609b242da1548cbc
Devin Wilson <craigvin84@yahoo.com>
2020-10-20 03:26:02 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,20 +4,20 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name:"Mercurito", age: "18", atitude: "hot-headed"}
         #your hashes here!
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{name: "Steven", age: "30", atitude: "confused"}, {name: "Nurse", age: "44", atitude: "worried"}
         #your hashes here!
       ]
    }
 }
 
-  
+
 
 end
